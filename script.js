d55846cd90b42ce3d40a166ba56e37c8f862cb4a
jobanraj <43281400+jobanraj@users.noreply.github.com>
2026-01-17 16:06:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+  anchor.addEventListener('click', function(e){
     e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({behavior:'smooth'});
   });
 });
